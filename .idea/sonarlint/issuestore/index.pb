523b2319ad72a28ce19afc88afec4aee7e32189c
Qihao He <qi.he@maine.edu>
2019-12-26 16:50:38 -0500

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+X
+(src/main/java/com/navin/Telusko/App.java,0/1/0182da95c7ae2c02e5064dc30f87f99e354df887
---
+++ src/main/java/com/navin/Telusko/Bike.java
@@ -0,0 +1,7 @@
+package com.navin.Telusko;
+
+public class Bike implements Vehicle{
+	public void drive() {
+		System.out.println("I am riding the bike!");
+	}
+}
